befe4d62611a73af02c8272b715ac7de5e8e37ee
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-03-13 13:29:05 +0200

    color

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,15 @@
+import styles from './home-page.module.css';
+
+
+
 const HomePage = () => {
   return (
-  <h3>HomePage</h3>
-)
+    <div className={styles.div}>
+      <h2 className={styles.title}>Welcome! 👍</h2>
+      <p className={styles.text}>
+        To get started, please login <span>🔑</span> or register📚
+      </p>
+    </div>
+  );
 };
 export default HomePage;
